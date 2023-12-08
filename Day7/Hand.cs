afd2b3597cc2f35f33552cd394bc8cd24a6fed5f
mariahhau <135694401+mariahhau@users.noreply.github.com>
2023-12-08 20:14:20 +0200

    Day 7 part 1

---
+++ Day7/Hand.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7Part1
+{
+    internal class Hand : IComparable<Hand>
+    {
+        string cards;
+        int bid;
+
+        public Hand(string cards, int bid)
+        {
+            this.cards = cards;
+            this.bid = bid;
+        }
+
+        public int GetBid() { return bid; }
+        public string GetCards() { return cards; }
+        public int CompareTo(Hand other)
+        {
+            if (other == null) return -1;
+
+            string otherHand = other.GetCards();
+
+            if (otherHand.Length < GetCards().Length) return -1;
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                int card = Hand.getIntValue(cards[i]);
+                int otherCard = Hand.getIntValue(other.cards[i]);
+
+                if (card < otherCard) return 1;
+                else if (card > otherCard) return -1;
+
+
+            }
+            return 0;
+        }
+        //Returns the hand: 0 == five of a kind ... 6 = high card
+        public int GetHand()
+        {
+            Dictionary<char, int> labels = new Dictionary<char, int>();
+
+            foreach (char card in cards)
+            {
+                if (labels.ContainsKey(card))
+                {
+                    labels[card]++;
+                }
+                else
+                {
+                    labels.Add(card, 1);
+                }
+
+            }
+            
+            int hand = -1;
+
+            switch (labels.Count)
+            {
+
+                case 1:
+                    hand = 0; //five of a kind AAAAA
+                    break;
+
+                case 2:
+                    {
+                        int max = labels.Values.Max();
+
+                        if (max > 3)
+                        {
+                            hand = 1; //four of a kind AA8AA
+                        }
+                        else
+                        {
+                            hand = 2; //full house AAA88 
+                        }
+
+                    }
+                    break;
+                case 3:
+                    {
+                        int max = labels.Values.Max();
+                        if (max > 2)
+                        {
+                            hand = 3; //three of a kind AAA98
+                            
+                        }
+                        else
+                        {
+                            hand = 4; //two pair A23A2
+                            
+                        }
+                    }
+                    break;
+                case 4:
+                    //one pair
+                    hand = 5;
+                    break;
+                case 5:
+                    //high card
+                    hand = 6;
+                    break;
+                default:
+                    break;
+            }
+
+            return hand;
+
+        }
+
+        public static int getIntValue(char card)
+        {
+            if (Char.IsNumber(card))
+            {
+                return int.Parse(card.ToString());
+            }
+            switch (card)
+            {
+                case 'A':
+                    return 14;
+                case 'K':
+                    return 13;
+                case 'Q':
+                    return 12;
+                case 'J':
+                    return 11;
+                case 'T':
+                    return 10;
+                default:
+                    return 0;
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day7Part1
+{
+    internal class Program
+    {   
+        static readonly string filename = "..\\..\\input.txt";
+
+        static void Main(string[] args){
+                 
+            string[] lines = File.ReadAllLines(filename);
+            List<Hand>[] hands = new List<Hand>[7];
+            
+            for (int i = 0; i < hands.Length; i++)
+            {
+                hands[i] = new List<Hand>();
+            }
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ');
+                Hand newHand = new Hand(parts[0], int.Parse(parts[1]));
+                int index = newHand.GetHand();
+                
+                if (hands[index] == null)
+                {
+                  hands[index] = new List<Hand>();
+                }
+                hands[index].Add(newHand);
+            
+            }
+
+            int rank = lines.Length;
+            int total = 0;
+            for (int i = 0; i < hands.Length; i++)
+            {
+                hands[i].Sort();
+                foreach(Hand hand in hands[i])
+                {
+                    total += rank * hand.GetBid();
+                    rank--;
+                }
+            }
+            Console.WriteLine("Total winnings: " + total);
+
+        }   
+
+       
+    }
+}
