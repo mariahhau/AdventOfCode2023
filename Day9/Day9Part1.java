b0115764df25f8cd7283319b08a6dc213f74aa9b
mariahhau <135694401+mariahhau@users.noreply.github.com>
2023-12-14 11:29:07 +0200

    Create Day9Part1.java

---
+++ Day9/Day9Part1.java
@@ -0,0 +1,77 @@
+package Day9;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day9Part1 {
+
+    public static void main(String[] args) {
+        List<List<Long>> lines = new ArrayList<>();
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader("Day9\\input.txt"));
+            String line = br.readLine();
+            while (null != line) {
+                List<Long> numbers = Stream.of(line.split(" ")).map(Long::valueOf).collect(Collectors.toList());
+                lines.add(numbers);
+                line = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        long sum = 0;
+
+        for (List<Long> line : lines) {
+            sum += calculateNext(line);
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+
+    public static long calculateNext(List<Long> list) {
+        if (list == null || list.size() < 1)
+            return -1;
+
+        if (list.size() == 1)
+            return list.get(0);
+
+        List<Long> newList = new ArrayList<>();
+        boolean zeroDiff = true;
+        Long firstDiff;
+
+        firstDiff = Long.valueOf(list.get(1) - list.get(0));
+        newList.addFirst(firstDiff);
+
+        for (int i = 1; i < list.size() - 1; i++) {
+            Long diff = Long.valueOf(list.get(i + 1) - list.get(i));
+            newList.add(diff);
+            if (diff != firstDiff) {
+                zeroDiff = false;
+            }
+        }
+
+        if (zeroDiff) {
+            return (list.get(list.size() - 1) + firstDiff);
+        } else {
+            return list.get(list.size() - 1) + calculateNext(newList);
+        }
+    }
+}
